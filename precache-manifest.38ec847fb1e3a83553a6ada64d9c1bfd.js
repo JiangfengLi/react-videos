cbe4cf4914a2e0661ee55ee618baf1a28db92c84
Jiangfeng Li <lijiangfeng2016@email.arizona.edu>
2020-08-01 12:00:07 -0700

    Updates

---
+++ precache-manifest.38ec847fb1e3a83553a6ada64d9c1bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0765b18f796956aae69bee6460c613",
+    "url": "/react-videos/index.html"
+  },
+  {
+    "revision": "adb635a4cb988aa8d1ff",
+    "url": "/react-videos/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "fd261265b20f23258768",
+    "url": "/react-videos/static/js/2.14624d3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-videos/static/js/2.14624d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb635a4cb988aa8d1ff",
+    "url": "/react-videos/static/js/main.0a9b1252.chunk.js"
+  },
+  {
+    "revision": "12093832f22bad87c231",
+    "url": "/react-videos/static/js/runtime-main.451339bf.js"
+  }
+]);
